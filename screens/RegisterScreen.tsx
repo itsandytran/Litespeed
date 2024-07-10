8c66f640f2aab6f99bc9ca34765a97ad5948d224
itsandytran <dev.andytran@gmail.com>
2024-07-10 00:09:34 -0400

    Created RegisterScreen.tsx

---
+++ screens/RegisterScreen.tsx
@@ -0,0 +1,20 @@
+import { View, Text, StyleSheet } from "react-native";
+
+import MenuPage from "../components/MenuPage";
+
+function RegisterScreen() {
+  return (
+    <View style={styles.register}>
+      <Text>Register</Text>
+      <MenuPage />
+    </View>
+  );
+}
+
+export default RegisterScreen;
+
+const styles = StyleSheet.create({
+  register: {
+    padding: 80,
+  },
+});
