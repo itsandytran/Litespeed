89612ee20836446d9dc0bad1e7f5d0c7ae0ee5c8
itsandytran <dev.andytran@gmail.com>
2024-07-13 15:47:42 -0400

    Implemented Order Summary time and total

--- components/OrderSummary.tsx
+++ components/OrderSummary.tsx
@@ -1,8 +1,18 @@
 import { View, Text, StyleSheet } from "react-native"
+import { useState, useEffect } from "react"
 
 import CheckOutButton from "./CheckOutButton"
 
 function OrderSummary() {
+  const [date, setDate] = useState(new Date())
+
+  useEffect(() => {
+    var timer = setInterval(() => setDate(new Date()), 1000)
+    return function cleanup() {
+      clearInterval(timer)
+    }
+  })
+
   return (
     <View style={styles.background}>
       <Text style={{ color: "white", fontSize: 16, padding: 8 }}>
@@ -18,18 +28,27 @@ function OrderSummary() {
         OrderItem 3
       </Text>
 
-      {/* Check-out buttons */}
-      <View style={styles.row}>
-        <CheckOutButton buttonName="Cash" />
-        <CheckOutButton buttonName="Card" />
-      </View>
-
-      {/* Cash Amount Prompt */}
-      <View style={styles.row}>
-        <CheckOutButton buttonName="39.90" />
-        <CheckOutButton buttonName="40" />
-        <CheckOutButton buttonName="50" />
-        <CheckOutButton buttonName="60" />
+      <View style={styles.checkOutContainer}>
+        <View style={styles.orderTotalRow}>
+          <Text style={styles.dateTimeText}>
+            {date.toLocaleString("en-CA", {
+              timeZone: "EST",
+              timeStyle: "short",
+              dateStyle: "medium",
+            })}
+          </Text>
+          <Text style={styles.orderTotalText}>39.90</Text>
+        </View>
+        <View style={styles.checkOutButtonsRow}>
+          <CheckOutButton buttonName="Cash" />
+          <CheckOutButton buttonName="Card" />
+        </View>
+        <View style={styles.checkOutButtonsRow}>
+          <CheckOutButton buttonName="39.90" />
+          <CheckOutButton buttonName="40" />
+          <CheckOutButton buttonName="50" />
+          <CheckOutButton buttonName="60" />
+        </View>
       </View>
     </View>
   )
@@ -42,8 +61,25 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#232429",
   },
-  row: {
+  checkOutContainer: {
+    backgroundColor: "#2F3135",
+    paddingBottom: 5,
+  },
+  checkOutButtonsRow: {
     flexDirection: "row",
     paddingHorizontal: 2,
+  },
+  orderTotalRow: {
+    flexDirection: "row",
+    padding: 20,
+    justifyContent: "space-between",
+  },
+  dateTimeText: {
+    color: "white",
+  },
+  orderTotalText: {
+    color: "white",
+    fontSize: 20,
+    fontWeight: "bold",
   },
 })
