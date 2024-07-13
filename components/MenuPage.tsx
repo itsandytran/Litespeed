d163232ddd8178e9dd9d3581ade7bacbda1426e0
itsandytran <dev.andytran@gmail.com>
2024-07-12 23:00:25 -0400

    Dynamically generate MenuItems from a list using map()

--- components/MenuPage.tsx
+++ components/MenuPage.tsx
@@ -2,22 +2,32 @@ import { View, Text, StyleSheet } from "react-native";
 
 import MenuItem from "./MenuItem";
 
+let menu: {
+  itemName: string
+  itemPrice: string
+  buttonColor: string
+}[] = [
+  { itemName: "Ham Sandwich", itemPrice: "16.00", buttonColor: "#6D9496" },
+  { itemName: "Tuna Sandwich", itemPrice: "21.00", buttonColor: "#e1b382" },
+  { itemName: "Steak Sandwich", itemPrice: "21.00", buttonColor: "#2d545e" },
+  { itemName: "Cheese Burger", itemPrice: "16.00", buttonColor: "#12343b" },
+  { itemName: "BLT", itemPrice: "16.00", buttonColor: "#ffaaab" },
+]
+
+
 function MenuPage() {
   return (
     <View style={styles.background}>
       <View style={styles.menuItemRow}>
-        <MenuItem itemName="Ham Sandwich" itemPrice="16.00" buttonColor="#aed6dc"/>
-        <MenuItem itemName="Tuna Sandwich" itemPrice="21.00" buttonColor="#aed6dc"/>
-        <MenuItem itemName="Steak Sandwich" itemPrice="21.00" buttonColor="#aed6dc"/>
-        <MenuItem itemName="Cheese Burger" itemPrice="16.00" buttonColor="#aed6dc"/>
-        <MenuItem itemName="Bacon Burger" itemPrice="16.00" buttonColor="#aed6dc"/>
-      </View>
-      <View style={styles.menuItemRow}>
-        <MenuItem itemName="Veggie Sandwich" itemPrice="9.00" buttonColor="#ff9a8d"/>
-        <MenuItem itemName="Cheese Veggie Sandwich" itemPrice="11.00" buttonColor="#ff9a8d"/>
-        <MenuItem itemName="Grilled Cheese" itemPrice="12.00" buttonColor="#ff9a8d"/>
-        <MenuItem itemName="Hotdog" itemPrice="12.00" buttonColor="#6D9496"/>
-        <MenuItem itemName="BLT" itemPrice="16.00" buttonColor="#6D9496"/>
+        {menu.map((item) => (
+          <View key={item.itemName}>
+            <MenuItem
+              itemName={item.itemName}
+              itemPrice={item.itemPrice}
+              buttonColor={item.buttonColor}
+            />
+          </View>
+        ))}
       </View>
     </View>
   );
