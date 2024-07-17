cd0b71cd500e341aa20328521d00a263ba4a38f9
Chengjie Mao <minnows.lodge_0s@icloud.com>
2024-07-17 19:05:27 -0400

    Update styles of `OrderItem`

--- components/OrderItem.tsx
+++ components/OrderItem.tsx
@@ -21,38 +21,55 @@ export const OrderItem: FC<OrderItemProps> = ({
   onDelete,
 }) => {
   return (
-    <View style={style.container}>
-      <Text aria-label="quantity" style={style.number}>
-        {quantity}
-      </Text>
-      <Text aria-label="name">{name}</Text>
-      <Text aria-label="price" style={style.number}>
-        {price}
-      </Text>
+    <View>
+      <View style={[style.orderItem]}>
+        <Text aria-label="quantity" style={[style.text, style.number]}>
+          {quantity}
+        </Text>
+        <Text aria-label="name" style={[style.text]}>
+          {name}
+        </Text>
+        <Text aria-label="price" style={[style.text, style.number]}>
+          {price}
+        </Text>
+      </View>
       <Pressable
         onPress={onDelete ? onDelete : () => {}}
         role="button"
         style={style.deleteButton}
       >
-        <Text>Delete</Text>
+        <Text
+          style={[
+            style.text,
+            {
+              color: "white",
+            },
+          ]}
+        >
+          Delete
+        </Text>
       </Pressable>
     </View>
   )
 }
 
 const style = StyleSheet.create({
-  container: {
+  orderItem: {
     display: "flex",
     flexDirection: "row",
     justifyContent: "space-between",
-    maxWidth: 250,
-    padding: 4,
-  },
-  number: {
-    flexBasis: 18,
-    textAlign: "right",
+    padding: 8,
   },
   deleteButton: {
     backgroundColor: "red",
+    justifyContent: "center",
+    padding: 8,
+  },
+  number: {
+    flexBasis: 32,
+    textAlign: "right",
+  },
+  text: {
+    fontSize: 22,
   },
 })
