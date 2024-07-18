533f672a5f2aab0211f9a793b309cab3f11e7c8a
Chengjie Mao <minnows.lodge_0s@icloud.com>
2024-07-18 17:04:29 -0400

    Add some sample data to `OrderSummary`

--- components/OrderSummary.tsx
+++ components/OrderSummary.tsx
@@ -1,9 +1,34 @@
 import { View, Text, StyleSheet } from "react-native"
 
 import CheckOutButtons from "./CheckOutButtons"
-import { OrderItem } from "./OrderItem"
+import { OrderItem, OrderItemDetails } from "./OrderItem"
 import Colors from "../constants/colors"
 import CustomerInstructions from "./CustomerInstructions"
+
+const sampleData: OrderItemDetails[] = [
+  {
+    name: "Poutine",
+    price: 7,
+    quantity: 1,
+    specialInstructions: "more gravy",
+  },
+  {
+    name: "Ham Burger",
+    price: 7.5,
+    quantity: 2,
+  },
+  {
+    name: "Ham Burger",
+    price: 7.5,
+    quantity: 1,
+    specialInstructions: "add tomato",
+  },
+  {
+    name: "Deep Fried Cheese Curds",
+    price: 10,
+    quantity: 1,
+  },
+]
 
 function OrderSummary() {
   return (
