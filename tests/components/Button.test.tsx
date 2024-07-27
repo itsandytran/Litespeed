7f6b01c801b677e84e8f9f1d6ed69137dc90dde1
Chengjie Mao <minnows.lodge_0s@icloud.com>
2024-07-27 08:56:44 -0400

    Create a general button component
    The `Button` component from React Native has limited support for custom styles. To have better styling, we need a custom button implemented using the `Pressable` component.

---
+++ tests/components/Button.test.tsx
@@ -0,0 +1,34 @@
+import "@testing-library/react-native/extend-expect"
+import { render, screen, userEvent } from "@testing-library/react-native"
+
+import Button from "@components/Button"
+
+describe("<Button />", () => {
+  const buttonText = "Test"
+  const component = <Button text={buttonText} />
+
+  it('has a role of "button"', () => {
+    render(component)
+    const button = screen.root
+    expect(button).not.toBeUndefined()
+    expect(button).toHaveProp("role", "button")
+  })
+
+  it("contains a text", () => {
+    render(component)
+    const button = screen.getByRole("button")
+    expect(button).toHaveTextContent(buttonText)
+  })
+
+  it("performs the action when pressed", async () => {
+    const action = jest.fn()
+    const component = <Button action={action} text={buttonText} />
+    render(component)
+
+    const button = screen.getByRole("button")
+    const user = userEvent.setup()
+    await user.press(button)
+
+    expect(action).toHaveBeenCalledTimes(1)
+  })
+})
