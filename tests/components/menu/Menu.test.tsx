0389f43df9b9e0b8f0cbd48f8285513b22627f9d
Chengjie Mao <minnows.lodge_0s@icloud.com>
2024-07-28 10:07:32 -0400

    Add test suites for some of the remaining components

---
+++ tests/components/menu/Menu.test.tsx
@@ -0,0 +1,6 @@
+import "@testing-library/react-native/extend-expect"
+import { render, screen } from "@testing-library/react-native"
+
+describe.skip("<Menu />", () => {
+  it("has many menu items", () => {})
+})
---
+++ tests/components/order/CustomerInstructions.test.tsx
@@ -0,0 +1,6 @@
+import "@testing-library/react-native/extend-expect"
+import { render, screen } from "@testing-library/react-native"
+
+describe.skip("<CustomerInstructions />", () => {
+  it('shows a placeholder of "Add note"', () => {})
+})
