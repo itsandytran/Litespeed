c7cd30d9eef5c9266369a7e8e9638984d21145e0
Andy Tran <dev.andytran@gmail.com>
2024-10-21 19:23:45 -0400

    Added new sample colours (yellow, orange, green, red)

--- constants/colors.tsx
+++ constants/colors.tsx
@@ -12,6 +12,11 @@ const Colors = {
   yellow1: "#FCC74A",
 
   backgroundGray: "#F2F2F7",
+  sample_yellow: "#FFA600",
+  sample_orange: "#FE8000",
+  sample_green : "#008B2C",
+  sample_red   : "#FC791B",
+
 }
 
 export default Colors
