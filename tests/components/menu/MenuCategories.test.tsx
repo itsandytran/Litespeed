db7d87097ca793b50d8cc1cf83f28f1da494496e
Andy Tran <dev.andytran@gmail.com>
2024-10-23 17:36:01 -0400

    Removed MenuCategories component (no longer being used).

--- tests/components/menu/MenuCategories.test.tsx
+++ tests/components/menu/MenuCategories.test.tsx
@@ -1,12 +1,12 @@
 import "@testing-library/react-native/extend-expect"
 import { render, screen } from "@testing-library/react-native"
 
-import { sampleMenuCategories } from "@lib/sample-data"
+import { MenuCategories } from "@lib/sample-data"
 import MenuCategories from "@components/menu/MenuCategories"
 
 describe("<MenuCategories />", () => {
   it("renders given menu categories", () => {
-    const categories = sampleMenuCategories
+    const categories = MenuCategories
     render(<MenuCategories categories={categories} />)
     const categoryButtons = screen.getAllByRole("button")
     expect(categoryButtons).toHaveLength(categories.length)
