53d57081ab3c4ff558e333184476732bfc9674ca
Chengjie Mao <minnows.lodge_0s@icloud.com>
2024-10-24 00:09:05 -0400

    Update `App.tsx` to run migrations upon launch

--- App.tsx
+++ App.tsx
@@ -1,6 +1,34 @@
-import CheckoutScreen from "./screens/CheckOutScreen"
+import { useEffect } from "react"
+
 import RegisterScreen from "./screens/RegisterScreen"
 
+import { drizzle } from "drizzle-orm/expo-sqlite"
+import { openDatabaseSync } from "expo-sqlite"
+import { useFonts } from "expo-font"
+import { useMigrations } from "drizzle-orm/expo-sqlite/migrator"
+import * as SplashScreen from "expo-splash-screen"
+import migrations from "@db/drizzle/migrations"
+
+SplashScreen.preventAutoHideAsync()
+const expo = openDatabaseSync("litespeed.sqlite")
+export const db = drizzle(expo)
+
 export default function App() {
+  const [fontLoaded, _] = useFonts({
+    Fraunces: require("./assets/fonts/fraunces/Fraunces-VariableFont_SOFT,WONK,opsz,wght.ttf"),
+  })
+
+  const { success: migrationSuccess } = useMigrations(db, migrations)
+
+  useEffect(() => {
+    if (fontLoaded && migrationSuccess) {
+      SplashScreen.hideAsync()
+    }
+  }, [fontLoaded, migrationSuccess])
+
+  if (!(fontLoaded && migrationSuccess)) {
+    return null
+  }
+
   return <RegisterScreen />
 }
