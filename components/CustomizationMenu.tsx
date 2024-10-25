0631138bf071cf674caa88ca9f6e7af328a8b563
Andy Tran <dev.andytran@gmail.com>
2024-10-25 19:09:39 -0400

    CustomizationMenu is now default export

--- components/CustomizationMenu.tsx
+++ components/CustomizationMenu.tsx
@@ -9,7 +9,6 @@ import {
 
 import { MenuItemType } from "@lib/sample-data"
 import Button from "./common/Button"
-import Colors from "@constants/colors"
 import MenuItem from "./menu/MenuItem"
 
 type CustomizationMenuProps = {
