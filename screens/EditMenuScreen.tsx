995d80c9dfae23169302a791cb96922793ff78ca
Andy Tran <dev.andytran@gmail.com>
2024-11-05 18:10:55 -0500

    Editing Panel and Menu Views Implemented with correct size ratio 0.4 -0.6

--- screens/EditMenuScreen.tsx
+++ screens/EditMenuScreen.tsx
@@ -1,37 +1,52 @@
-import { FC } from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { sampleMenuItems } from "@lib/sample-data";
-import Menu from "@components/menu/Menu";
-import Colors from "@constants/colors";
-import TitleBar from "@components/TitleBar";
-import textStyles from "@constants/textStyles";
+import { FC } from "react"
+import { StyleSheet, Text, View } from "react-native"
+import { sampleMenuItems } from "@lib/sample-data"
+import Menu from "@components/menu/Menu"
+import Colors from "@constants/colors"
+import TitleBar from "@components/TitleBar"
+import textStyles from "@constants/textStyles"
 
 export type EditMenuProps = {
   greeting: string
-};
+}
 
 const EditMenuScreen: FC<EditMenuProps> = (greeting) => {
   return (
     <View style={styles.backgroundContainer}>
       <TitleBar />
-      <View>
-        <Text style={textStyles.header}>Editing Panel</Text>
-      </View>
+      <View style={styles.editPanel_Menu_Container}>
+        {/* Edit Menu Panel */}
+        <View style={styles.editPanelContainer}>
+          <Text style={textStyles.header}>Editing Panel</Text>
+        </View>
 
-      {/* Menu Grid  */}
-      <View>
-        <Text style={textStyles.regular}>Menu</Text>
-        <Text style={textStyles.italic}> Hello world! </Text>
+        {/* Menu Grid */}
+        <View style={styles.menuContainer}>
+          <Text style={textStyles.regular}>Menu</Text>
+          <Text style={textStyles.italic}> Hello world! </Text>
+        </View>
       </View>
     </View>
-  );
-};
+  )
+}
 
-export default EditMenuScreen;
+export default EditMenuScreen
 
 const styles = StyleSheet.create({
   backgroundContainer: {
     backgroundColor: Colors.backgroundGray,
     flex: 1,
   },
-});
+  editPanel_Menu_Container: {
+    flexDirection: "row",
+    flex: 1,
+  },
+  editPanelContainer: {
+    backgroundColor: "red",
+    flex: 0.4
+  },
+  menuContainer: {
+    backgroundColor: "blue",
+    flex: 0.6
+  }
+})
